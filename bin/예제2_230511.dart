d0db6564babec18208166bb85a3d7c4af831efa8
Astrum <jungmin2670@gmail.com>
2023-05-11 15:54:49 +0900

    230511

---
+++ bin/예제2_230511.dart
@@ -0,0 +1,16 @@
+void main() {
+  final String name = 'NA';
+
+  late final String email;
+  // do something, go to api //
+  email = 'na@na.com';
+  print(email);
+
+  ///fianl과 같다.///
+  // const name1 = 'na';
+
+  // API에서 받아 와야 하기 때문에 불가능 //
+  // const API= fetchApi();
+
+  const max_allowed_price = 120;
+}
